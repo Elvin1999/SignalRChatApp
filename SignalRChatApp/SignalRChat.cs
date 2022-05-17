bcf83664629902fdb0dda2e0f5d209db2d02d503
Elvin Camalzade <e.camalzade@itstep.edu.az>
2022-05-17 09:45:40 +0400

    Add project files.

---
+++ SignalRChatApp/SignalRChat.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatApp
+{
+    public class SignalRChat :Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",user, message);
+        }
+    }
+}
